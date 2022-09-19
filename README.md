580fffa1721149b8d763192386da6a2951bdaea4
ProfessorNik <90216842+ProfessorNik@users.noreply.github.com>
2022-09-20 03:33:09 +0700

    added readme file
    - form factor check

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Computer shop
- 
+Task for shift lab
+
+# Tools
+
+1. Java 17.0.3
+2. Gradle 7.5
+
+# How to Run
+
+gradle bootRun
--- src/main/java/balyasnikov/nikolay/computerstore/domain/entity/DesktopComputer.java
+++ src/main/java/balyasnikov/nikolay/computerstore/domain/entity/DesktopComputer.java
@@ -5,10 +5,13 @@ import lombok.NonNull;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import java.util.List;
+import java.util.Locale;
 
 @Entity
 @Table(name = "DESKTOP_COMPUTER")
 public class DesktopComputer extends Product {
+    private static List<String> availableFormFactors = List.of("desktop", "monoblock", "nettop");
     @Column(name = "form_factor")
     private String formFactor;
 
@@ -17,6 +20,9 @@ public class DesktopComputer extends Product {
     }
 
     public void setFormFactor(@NonNull String formFactor) {
+        if(availableFormFactors.contains(formFactor.toLowerCase(Locale.ROOT))){
+            throw new IllegalArgumentException("From factor of the computer can be one of: " + availableFormFactors);
+        }
         this.formFactor = formFactor;
     }
 }
